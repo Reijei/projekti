188d44c6f2279886590d5baa8a2957a13569e225
velli666 <vippuurunen@gmail.com>
2018-11-13 20:02:39 +0200

    asd
    asd

--- edit.php
+++ edit.php
@@ -100,7 +100,7 @@ if (isset($_SESSION['id'])) {// tarkistetaan id
     </body>
 </html>
 <?php
-    } else { // heittää ulos jos ideetä ei ole sessionissaasd
+    } else { // heittää ulos jos ideetä ei ole sessionissaasdasd
         header('Location: login.php');
         exit;
 ?>  
