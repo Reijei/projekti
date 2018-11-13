1ce1573da8c07d0b2703fd159e4c6fe91120791c
Pasi_H <44438800+pasi-h@users.noreply.github.com>
2018-11-14 01:19:08 +0200

    Etusivu + muuta kohellusta

---
+++ etusivu.php
@@ -0,0 +1,25 @@
+<html>
+<meta charset="UTF-8">
+	<head>
+		<title>Etusivu</title>
+		  <link rel="stylesheet" type="text/css" href="styless.css">
+	</head>
+<body>
+<div class="login">
+<a href="http://localhost/projekti3/login.php">Kirjaudu sisään</a>
+</div>
+<div class="tilaa">
+<a href="http://localhost/projekti3/asiakastilaus.php">Tilaa puhdistin meiltä</a>
+</div>
+	<div class="etusivu">
+	
+		
+		<img src="lgo.png" alt="logo">
+		<h1>Tervetuloa!</h1>
+	
+	</div>
+	
+
+
+</body>
+</html>
